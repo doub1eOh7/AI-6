af320a22466c9df9ca8f5352486bafda027cc9b8
bab010 <bab010@admins-mbp>
2017-04-03 20:07:29 -0500

    Decision Tree is outperforming baseline

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,14 +1,38 @@
 import java.util.Random;
 
-class Node {
+abstract class Node {
+	Matrix x; 
+	Matrix y;
+	Node a;
+	Node b;
+	double splitval;
+	int splitcol;
+	boolean isCat;
+	abstract boolean isInteriorNode();
 	
 }
 
-class InternalNode extends Node {
+class InteriorNode extends Node {
 	
+	boolean isInteriorNode()
+	{
+		return true;
+	}
 }
 
-class LeafNode extends InternalNode {
+class LeafNode extends Node {
+	double[] label;
+	
+	LeafNode(Matrix a, Matrix b)
+	{
+		this.x = a;
+		this.y = b;
+	}
+	
+	boolean isInteriorNode()
+	{
+		return false;
+	}
 	
 }
 
@@ -23,14 +47,130 @@ public class DecisionTree extends SupervisedLearner {
 	
 	Node buildTree(Matrix features, Matrix labels)
 	{
-		Random rand = new Random();
+		int seed = 1;
+		Random rand = new Random(seed);
 		int splitcol = rand.nextInt(features.cols());
 		int randrow = rand.nextInt(features.rows());
 		double splitval = features.row(randrow)[splitcol];
+		Matrix af = new Matrix();
+		Matrix bf = new Matrix();
+		Matrix al = new Matrix();
+		Matrix bl = new Matrix();
+
+		if(features.valueCount(splitcol) == 0)
+		{
+			for(int attempts = 0; attempts < 10; attempts++)
+			{
+				splitcol = rand.nextInt(features.cols());
+				randrow = rand.nextInt(features.rows());
+				splitval = features.row(randrow)[splitcol];
+
+				af.copyMetaData(features);
+				bf.copyMetaData(features);
+				al.copyMetaData(labels);
+				bl.copyMetaData(labels);
+				
+				for(int i = 0; i < features.rows(); i++)
+				{
+					if(features.row(i)[splitcol] < splitval)
+					{
+						Vec.copy(af.newRow(), features.row(i));
+						Vec.copy(al.newRow(), labels.row(i));
+					}
+					else
+					{
+						Vec.copy(bf.newRow(), features.row(i));
+						Vec.copy(bl.newRow(), labels.row(i));
+					}
+				}
+				if(af.rows() > 0 && bf.rows() > 0)
+					break;
+			}
+			
+			if(af.rows() < 1)
+			{
+				LeafNode l = new LeafNode(bf, bl);
+				l.label = bl.row(0);
+				//System.out.println(l.toString());
+				return l;
+			}
+			if(bf.rows() < 1)
+			{
+				LeafNode l = new LeafNode(af, al);
+				l.label = al.row(0);
+				//System.out.println(l.toString());
+				return l;
+			}
+			
+			InteriorNode n = new InteriorNode();
+			n.splitval = splitval;
+			n.splitcol = splitcol;
+			n.a = buildTree(af, al);
+			n.b = buildTree(bf, bl);
+			n.x = af;
+			n.y = bf;
+			
+			//System.out.println(n.toString());
+			return n;
+		}
 		
-		while()
-		
-		return new Node()
+		else
+		{
+			for(int attempts = 0; attempts < 10; attempts++)
+			{
+				splitcol = rand.nextInt(features.cols());
+				randrow = rand.nextInt(features.rows());
+				splitval = features.row(randrow)[splitcol];
+
+				af.copyMetaData(features);
+				bf.copyMetaData(features);
+				al.copyMetaData(labels);
+				bl.copyMetaData(labels);
+			
+				for(int i = 0; i < features.rows(); i++)
+				{
+					if(features.row(i)[splitcol] == splitval)
+					{
+						Vec.copy(af.newRow(), features.row(i));
+						Vec.copy(al.newRow(), labels.row(i));
+					}
+					else
+					{
+						Vec.copy(bf.newRow(), features.row(i));
+						Vec.copy(bl.newRow(), labels.row(i));
+					}
+				}
+			
+				if(af.rows() > 0 && bf.rows() > 0)
+					break;
+			}
+				
+				
+			if(af.rows() < 1)
+			{
+				LeafNode l = new LeafNode(bf, bl);
+				l.label = bl.row(0);
+				//System.out.println(l.toString());
+				return l;
+			}
+			if(bf.rows() < 1)
+			{
+				LeafNode l = new LeafNode(af, al);
+				l.label = al.row(0);
+				//System.out.println(l.toString());
+				return l;
+			}
+			
+			InteriorNode n = new InteriorNode();
+			n.splitval = splitval;
+			n.splitcol = splitcol;
+			n.a = buildTree(af, al);
+			n.b = buildTree(bf, bl);
+			n.x = af;
+			n.y = bf;
+			//System.out.println(n.toString());
+			return n;
+		}
 	}
 	
 	void train(Matrix features, Matrix labels)
@@ -40,9 +180,29 @@ public class DecisionTree extends SupervisedLearner {
 
 	void predict(double[] in, double[] out)
 	{
+		Node n = (InteriorNode)root;
+		while(n.isInteriorNode())
+		{
+			if(n.x.valueCount(n.splitcol) == 0)
+			{
+				if(in[n.splitcol] < n.splitval)
+					n = n.a;
+				else
+					n = n.b;
+			}
+			else
+			{
+				if(in[n.splitcol] == n.splitval)
+					n = n.a;
+				else
+					n = n.b;
+			}
+		}
 		
-	}
-	
+		LeafNode leafnode = (LeafNode)n;
+		Vec.copy(out, leafnode.label);
+		
+	}	
 	
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,7 @@ class Main
 	public static void main(String[] args)
 	{
 		testLearner(new BaselineLearner());
+		testLearner(new DecisionTree());
 		//testLearner(new RandomForest(50));
 	}
 }
